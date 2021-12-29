02750ca8bc6fdca4d8b0cd31f2d91cef5b25b8e0
Kostiantyn Bondarenko <kbonda@softserveinc.com>
2021-12-29 13:34:09 +0200

    init

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ import jQuery    from "jquery"
 window.$ = window.jQuery = jQuery
 
 const history = createHistory()
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+App.use(cors(corsOptions));
 
 ReactDOM.render(
     <Provider store={STORE}>
